b13e3013cd5e4859c49ecdf134a9d575f9152b7d
AESnegireva <86655071+AESnegireva@users.noreply.github.com>
2021-07-01 12:10:33 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -1,13 +1,12 @@
 # Напишите функцию, возвращающую наибольшее из двух чисел
-
-def max2(n1, n2):
-    # TODO: your code here
-    pass
-
-
-# Тестируем функцию
-print(max2(5, 6))
-print(max2(-10, -12))
-print(max2(2.5, 2.6))
-print(max2(-2.5, 0))
-print(max2(0, -2.5))
+def my_max(n1, n2):
+    if n1<n2:
+        return n2
+    else:
+        return n1
+print(my_max(5, 6))
+print(my_max(-10, -12))
+print(my_max(2.5, 2.6))
+print(my_max(-2.5, 0))
+print(my_max(0, -2.5))
+print(my_max(5, 5))
